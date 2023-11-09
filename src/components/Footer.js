3bb5d4c62569816e65989c07a238a3ffb9299f60
AkhibRahi <akhibrahi786@gmail.com>
2023-11-09 06:44:54 +0530

    footer section created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,55 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-black text-white py-16 text-center'>
+      <div className='max-w-[1240px] px-4 mx-auto grid grid-cols-2 lg:grid-cols-5'>
+        <div className=''>
+          <h3 className='border-b inline-block border-[#00d8ff] mt-4 text-xl font-bold'>Products</h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+        <div className=''>
+          <h3 className='border-b inline-block border-[#00d8ff] mt-4 text-xl font-bold'>Products</h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+        <div className=''>
+          <h3 className='border-b inline-block border-[#00d8ff] mt-4 text-xl font-bold'>Products</h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+        <div className=''>
+          <h3 className='border-b inline-block border-[#00d8ff] mt-4 text-xl font-bold'>Products</h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+        <div className=''>
+          <h3 className='border-b inline-block border-[#00d8ff] mt-4 text-xl font-bold'>Products</h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
